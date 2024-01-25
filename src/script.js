96f22813f9b4826edaa546e100508bc4da1e201f
MKPW27 <66383090+MKPW27@users.noreply.github.com>
2024-01-25 14:56:22 +0800

    animations / projects

--- src/script.js
+++ src/script.js
@@ -2,12 +2,33 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
         }
     });
 });
 
-const hiddenElements = document.querySelectorAll('.hidden, .list'); 
-hiddenElements.forEach((e) => observer.observe(e)); 
+const hiddenElements = document.querySelectorAll('.hidden, .list, .speech-bubble, .wrapper, .project-card');
+hiddenElements.forEach((e) => {
+    observer.observe(e);
+    e.classList.remove('show');
+});
+
+jQuery(function() {
+    jQuery('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+  
+        var target = jQuery(this.hash);
+        target = target.length ? target : jQuery('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          jQuery('html,body').animate({
+            scrollTop: target.offset().top -100
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
 
 // const nav = document.querySelector("nav")
 // let lastSrollY = window.scrollY
